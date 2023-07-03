0d61b27fd9928274e8438ca9d21dd9c0261e5435
Userasma <136679245+Userasma@users.noreply.github.com>
2023-07-03 20:59:06 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/Asma/go-crud/controllers"
+	"github.com/Asma/go-crud/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/posts", controllers.PostCreate)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.GET("/posts", controllers.PostIndex)
+	//this work as a dynamic router
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.DELETE("/posts/:id", controllers.PostDelete)
+
+	r.Run()
+}

12d6505de4933318358cd8ddddd46ce557692d21
Userasma <136679245+Userasma@users.noreply.github.com>
2023-07-25 20:04:12 +0400

    database update

--- models/postModels.go
+++ models/postModels.go
@@ -4,19 +4,20 @@ import "gorm.io/gorm"
 
 type Post struct {
 	gorm.Model
+	id        int
 	FirstName string
 	LastName  string
 	//Email string
 }
 
-type member struct {
-	gorm.Model
-	first_name  string
-	second_name string
-	email       string
-}
-type booking struct {
-	gorm.Model
-	member_id      int
-	tickets_number int
-}
+// type member struct {
+// 	gorm.Model
+// 	first_name  string
+// 	second_name string
+// 	email       string
+// }
+// type booking struct {
+// 	gorm.Model
+// 	member_id      int
+// 	tickets_number int
+// }
